ecfce435f8fd1e2924a5bf82b040dddd5aaa4503
Laminatqw <victor.kinash47@gmail.com>
2024-04-30 18:05:51 +0300

    hw1

---
+++ hw1/main.js
@@ -0,0 +1,31 @@
+//Pt1
+{
+    let a = 'hello';
+    let b = 'owu';
+    let c = 'com';
+    let d = 'ua';
+    let e = 1;
+    let f = 10;
+    let g = -999;
+    let h = 123;
+    let i = 3.14;
+    let j = 2.7;
+    let k = 16;
+    let l = true;
+    let m = false;
+    console.log(a, b, c, d, e, f, g, h, i, j, k, l, m);
+}
+//Pt2---------------------------------------------------------------
+let firstname = 'Victor';
+let lastname = 'Hzhondzel';
+let middlename = 'Rostislavovich'
+let person = `${firstname} ${middlename} ${lastname}`;
+console.log(person);
+//Pt3----------------------------------------------------------------
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof(a), typeof(b), typeof(c) )
+
+
+
