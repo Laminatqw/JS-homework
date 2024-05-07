5ba636de752efc1a59b3bd07eb7d129167394931
Laminatqw <victor.kinash47@gmail.com>
2024-05-07 16:49:48 +0300

    hw3

--- hw3/main.js
+++ hw3/main.js
@@ -21,11 +21,11 @@
 // }
 //--------------------------------
 // let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+// document.write(`<ul>`);
 //     for (let item of listOfItems){
-//         document.write(`<ul>
-//                         <li>${item}</li>
-//                         </ul>`)
-//     }
+//         document.write(`<li>${item}</li>`);
+//    }
+// document.write(`</ul>`);
 //------------------------------------
 // let products = [
 //     {
